09d3271f53e7dcacc98d86c7d19288b720843798
manojshivamat <mgshivamat@gmail.com>
2021-03-04 16:02:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -57,10 +57,10 @@
     tableau.registerConnector(myConnector);
 
     // Create event listeners for when the user submits the form
-    $(document).ready(function() {
-        $("#submitButton").click(function() {
-            tableau.connectionName = "USA Population Data"; // This will be the data source name in Tableau
-            tableau.submit(); // This sends the connector object to Tableau
-        });
-    });
+    //$(document).ready(function() {
+        //$("#submitButton").click(function() {
+          //  tableau.connectionName = "USA Population Data"; // This will be the data source name in Tableau
+            //tableau.submit(); // This sends the connector object to Tableau
+        //});
+    //});
 })();
