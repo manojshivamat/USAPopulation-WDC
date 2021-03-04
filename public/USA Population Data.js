78de813f81073327b7da0e19e629692b6093c00d
manojshivamat <mgshivamat@gmail.com>
2021-03-04 17:46:24 +0530

    Update USA Population Data.js

--- public/USA Population Data.js
+++ public/USA Population Data.js
@@ -4,6 +4,30 @@ console.log("This is working");
     var myConnector = tableau.makeConnector();
 
     myConnector.getSchema = function (schemaCallback) {
+        const cols = [
+        {
+            id: "Nation",
+            alias: "Nation",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "Year",
+            alias: "Year",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "Population",
+            alias: "Population",
+            dataType: tableau.dataTypeEnum.string
+        },
+        ];
+
+        let tableSchema = {
+            id: "USAPopData",
+            alias: "USA Population",
+            columns: cols,
+        };
+
+        schemaCallback([tableSchema]);
+
 
     };
 
